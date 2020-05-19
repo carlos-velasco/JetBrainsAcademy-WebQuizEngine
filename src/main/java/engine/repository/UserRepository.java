113759cae67257e1ae2335584fc04190f54c6900
Carlos Velasco Nino <cvelnin@gmail.com>
2020-05-19 23:52:05 +0200

    Use CrudRepository instead of PagingAndSortingRepository for UserRepository

--- src/main/java/engine/repository/UserRepository.java
+++ src/main/java/engine/repository/UserRepository.java
@@ -1,11 +1,11 @@
 package engine.repository;
 
 import engine.model.user.User;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository("userRepository")
-public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+public interface UserRepository extends CrudRepository<User, Long> {
 
     User findByEmail(String email);
 }
