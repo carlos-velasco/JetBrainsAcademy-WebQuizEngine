f973121cef66387addc6da0d0864f1b478433612
Carlos Velasco Nino <cvelnin@gmail.com>
2020-09-05 19:28:32 +0200

    Remove mapper warning

--- src/main/java/engine/model/mapper/QuizMapper.java
+++ src/main/java/engine/model/mapper/QuizMapper.java
@@ -3,11 +3,14 @@ package engine.model.mapper;
 import engine.model.dto.QuizDto;
 import engine.model.quiz.Quiz;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface QuizMapper {
 
     QuizDto toQuizDto(Quiz quiz);
 
+    @Mapping(target = "quizCompletions", ignore = true)
+    @Mapping(target = "user", ignore = true)
     Quiz toQuiz(QuizDto quizDto);
 }
