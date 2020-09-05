b8e059293d4d60f9492536c481da35b77445e475
Carlos Velasco Nino <cvelnin@gmail.com>
2020-09-05 19:58:34 +0200

    Remove validation annotations from DTO class

--- src/main/java/engine/dto/QuizDto.java
+++ src/main/java/engine/dto/QuizDto.java
@@ -3,8 +3,6 @@ package engine.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Size;
 import java.util.List;
 import java.util.Set;
 
@@ -16,14 +14,9 @@ import java.util.Set;
 public class QuizDto {
 
     private Long id;
-
-    @NotEmpty
     private String title;
-
-    @NotEmpty
     private String text;
 
-    @Size(min = 2, message = "must have size equal or greater than 2")
     @Builder.Default
     private final List<String> options = List.of();
 
